c29c2f660b1ff469ee299888ef332ce047075162
anandraguraman <113701095+anandraguraman@users.noreply.github.com>
2022-10-04 08:56:41 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-current_temperature = input.temperature()
-basic.show_number(current_temperature)
-basic.pause(1000)
-basic.clear_screen()
+def on_button_pressed_a():
+    global counter
+    counter += 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_number(0)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global counter
+    counter += 1
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+counter = 0
--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
-let current_temperature = input.temperature()
-basic.showNumber(current_temperature)
-basic.pause(1000)
-basic.clearScreen()
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    counter += 1
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    counter += 1
+})
+let counter = 0
